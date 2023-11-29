31c3a418804d4ccd4e9548f0c50549e59391bcb2
Mariia Solo <78789270+mariiasolo@users.noreply.github.com>
2023-11-29 18:15:28 +0200

    Update Icons.js

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -563,7 +563,7 @@ export const LinkArrow = ({ className, ...rest }) => (
     width={24}
     height={24}
     viewBox="0 0 24 24"
-    className={`w-full h-auto ${className}`}
+    className={`w-6 h-6 ${className}`}
     {...rest}
   >
     <path fill="none" d="M0 0h24v24H0z" />
